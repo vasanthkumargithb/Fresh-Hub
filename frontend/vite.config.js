47d2daa68181532ef62d0afc9abdfc5bae71d82d
vasanthkumargithb <vasuvasanthvasu4@gmail.com>
2025-10-05 21:35:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,35 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+//     proxy:{
+//         "/api":{
+//           target:"http://localhost:5000"
+//         }
+//     }
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-        "/api":{
-          target:"http://localhost:5000"
-        }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
     }
   }
 })
