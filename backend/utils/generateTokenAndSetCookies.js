a14777fcf09143470f82ac8cfe74ef2a08af23f1
vasanthkumargithb <vasuvasanthvasu4@gmail.com>
2025-10-08 12:39:58 +0530

    Update generateTokenAndSetCookies.js

--- backend/utils/generateTokenAndSetCookies.js
+++ backend/utils/generateTokenAndSetCookies.js
@@ -8,7 +8,8 @@ export const generateTokenAndSetCookie = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    path: "/",
   });
 };
