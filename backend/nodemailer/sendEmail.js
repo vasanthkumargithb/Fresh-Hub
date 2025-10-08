2e49bf57473966d1b4f18c4a87f9382b19d425da
vasanthkumargithb <vasuvasanthvasu4@gmail.com>
2025-10-08 12:36:26 +0530

    Update sendEmail.js

--- backend/nodemailer/sendEmail.js
+++ backend/nodemailer/sendEmail.js
@@ -74,3 +74,4 @@ export const sendDeleteEmail = async (email, userName) => {
     const text = `Dear ${userName}, your account has been deleted.`;
     await sendMail(email, subject, text, html);
 };
+ 
